517ec98ce1cd90cedd735528798e7e6cdc62bf03
Hitesh Gorantla <74699575+GSHitesh@users.noreply.github.com>
2022-08-05 12:15:31 +0530

    Time: 33 ms (6.21%), Space: 9.5 MB (18.61%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        unordered_set<ListNode*> seen;
+        while (head) {
+            if (seen.find(head) != seen.end()) return head;
+            seen.insert(head);
+            head = head->next;
+        }
+        return NULL;
+    }
+};
