d4c9727640295793d4bd28c73d2d106c440ad0cd
Hitesh Gorantla <74699575+GSHitesh@users.noreply.github.com>
2022-08-11 23:53:49 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (30.48%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();   // elements in a row
+        int left = 0,right = m-1;   // rows wise
+        int top = 0, down = n-1;    // column wise
+        int direction = 1;          // default left to right
+        
+        vector <int> spiral;
+        
+        while(left <= right && top <= down){
+            if( direction == 1){    // left to right
+                for(int i=left;i<=right;i++){
+                    spiral.push_back(matrix[top][i]); // top will be fixed then top -> next
+                    
+                }
+                    top++;
+                    direction = 2;
+            }
+            
+            else if(direction == 2){     // top to down
+                for(int i=top;i<=down;i++){
+                    spiral.push_back(matrix[i][right]); // right will be fixed
+                    
+                }
+                
+                right--;
+                direction = 3;
+                
+            }
+            else if(direction == 3){     // right to left
+                for(int i=right;i>=left;i--){
+                    spiral.push_back(matrix[down][i]);  // down is fixed
+                    
+                }
+                
+                down--;
+                direction = 4;
+                
+            }
+            else if(direction == 4){     // down to top
+                for(int i=down;i>=top;i--){
+                    spiral.push_back(matrix[i][left]);  // left is fixed
+                    
+                }
+                
+                left++;
+                direction = 1;
+                
+            }
+        }
+        
+        
+        return spiral;
+    }
+};
